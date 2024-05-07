bb6cf4bead843ca58dc83405dbf20b24360353ed
EdgarAcevedoAcosta <jorge.verduzco247253@potros.itson.edu.mx>
2024-05-07 12:19:25 -0700

    reseña prueba 2

--- FuncionalidadPagoPorTarjeta/src/main/java/PagoPorTarjeta/FuncionalidadPagoPorTarjeta.java
+++ FuncionalidadPagoPorTarjeta/src/main/java/PagoPorTarjeta/FuncionalidadPagoPorTarjeta.java
@@ -3,12 +3,19 @@ package PagoPorTarjeta;
 
 
 import DTO.PagoPorTarjetaDTO;
-import INegocio.IComprarProductoBO;
+import INegocio.IPagoBO;
+import INegocio.IProductoBO;
 import IPagoPorTarjeta.IPagoPorTarjeta;
+import Negocio.PagoBO;
 
 public class FuncionalidadPagoPorTarjeta implements IPagoPorTarjeta{
     
-    IComprarProductoBO comprarProductoBO;
+    IPagoBO comprarProductoBO;
+
+    public FuncionalidadPagoPorTarjeta() {
+        comprarProductoBO= new PagoBO();
+    }
+    
     
     @Override
     public void comprarProductoPorTarjeta(PagoPorTarjetaDTO pagoPorTarjetaDTO) {
